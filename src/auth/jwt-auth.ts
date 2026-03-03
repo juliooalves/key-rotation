c9409d8aab6da4c65a75e300265d18cc6d5002b5
julindoti <julinmestredoti@gmail.com>
2026-03-03 15:16:05 -0300

    feat(vault): Add vault and backend integration
    With this integration now is  possible to pick authenticate user data, sended via payload, and create a JWT token signed with asymmetric encription keys stored on the vaul server

--- src/auth/jwt-auth.ts
+++ src/auth/jwt-auth.ts
@@ -1,30 +1,43 @@
-import jwt from "jsonwebtoken";:w
-import vault from "node-vault"
-import vaultClient from "../vault.ts"
+import jwt from "jsonwebtoken";
+import vault from "node-vault";
+import vaultClient from "../vault.ts";
 
-async function getUserToken (userData: {id: string, email: string}): Promise<string>{
-
-  try{
-    await vaultClient.write(`identity/entity/name/${userData.id}`,
-    metadata :{
-        email: userData.email, 
-        session_id: Date.now().toString()
-      });
-    const tokenRequest = await vaultClient.write("identity/token/create",{
-      entity_alias: userData, 
-      ttl: "5m" 
+export default async function getUserToken(userData: {
+  id: string;
+  email: string;
+}): Promise<string> {
+  try {
+    await vaultClient().write(`identity/entity/name/${userData.id}`, {
+      metadata: {
+        email: userData.email,
+        session_id: "1234",
+      },
     });
-    const vaultUserToken = tokenRequest.auth.client_token;
+    console.log("passed on the write identity endpoint");
+    const tokenRequest = await vaultClient().write(
+      "auth/token/create/jwt-issuer",
+      {
+        entity_alias: userData.id,
+        ttl: "5m",
+      },
+    );
+    console.log("token request successfully created", tokenRequest);
+    const vaultUserToken = await tokenRequest.auth.client_token;
     const vaultUserClient = vault({
       endpoint: "http://localhost:8200",
       token: vaultUserToken,
-    })
-    const result = vaultUserClient.read("identity/iodc/roles/user-role")
-    console.log("passed on user token getter", result.data.token)
-    return result.data.token
-    )
-  }catch(err){
-    console.error("Error: Server failed to load user token on vault,  error:", err)
-    return
+    });
+
+    console.log("user vault client created.");
+    const result = await vaultUserClient.read("identity/oidc/token/user-role");
+    console.log("result", result);
+    console.log("passed on user token getter", result.data.token);
+    return result.data.token;
+  } catch (err) {
+    console.error(
+      "Error: Server failed to load user token on vault,  error:",
+      err,
+    );
+    return;
   }
-} 
+}
--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,29 @@ import express, {
 } from "express";
 import * as dotenv from "dotenv";
 import dataSigner from "./data-signer.ts";
+import getUserToken from "./auth/jwt-auth.ts";
+
+interface UserData {
+  id: string;
+  email: string;
+}
 
 dotenv.config();
+
 const app: Application = express();
 const port = 3000;
 const payload = {
-  username: "julio",
-  sessionID: 746746,
+  id: "783rht",
+  email: "lester123@gmail.com",
 };
+app.use(express.json());
 app.get("/test", (req: Request, res: Response) => {
   res.status(200).json({ message: "everything is up" });
 });
 app.post("/user-auth", async (req: Request, res: Response) => {
   try {
     const signature = await dataSigner(payload);
-    res.status(200).json({ signature: signature });
+    res.status(200).json({ signature: sinature });
   } catch (err) {
     console.error("Error: Internal server error:", err);
     res
@@ -27,6 +35,22 @@ app.post("/user-auth", async (req: Request, res: Response) => {
       .send("Something went wrong with the request. Please try again later");
   }
 });
+app.post(
+  "/jwt-auth",
+  async (req: Request<unknown, unknown, UserData>, res: Response) => {
+    try {
+      const data = req.body;
+      const token = await getUserToken(data);
+      res.status(200).json({ user_id: `${payload.id}`, token: token });
+    } catch (err) {
+      console.error("Error: internal server error on JWT Auth:", err);
+      res.status(500).json({
+        message: "Failed to load the JWT Auth endpoint correctly",
+        error: err,
+      });
+    }
+  },
+);
 
 app.listen(port, () => {
   console.log("Server is up on port", port);
